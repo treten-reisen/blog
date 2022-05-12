c9fd5185407201daf732cb0ff38910b77515618e
Patrick Simmelbauer <p.simmelbauer@slashwhy.de>
2022-05-12 09:48:53 +0200

    add index page

---
+++ packages/frontend/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const NotFoundPage = () => (
+  <Layout>
+    <Seo />
+    <h1>treten.reisen</h1>
+    <p>Willkommen bei treten.reisen</p>
+  </Layout>
+)
+
+export default NotFoundPage
