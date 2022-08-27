c41705c34ffeadb3688d871822793dcd506fbcbe
Patrick Simmelbauer <patrick.simmelbauer@gmail.com>
2022-08-27 17:56:01 +0200

    remove unused liva-location routes

--- packages/strapi/src/api/live-location/routes/live-location.js
+++ packages/strapi/src/api/live-location/routes/live-location.js
@@ -6,4 +6,4 @@
 
 const { createCoreRouter } = require("@strapi/strapi").factories;
 
-module.exports = createCoreRouter("api::live-location.live-location");
+module.exports = createCoreRouter("api::live-location.live-location", { only: ["find"] });
