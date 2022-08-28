de0a712e43f052b58b02a88197c2cc5c677f5a65
Patrick Simmelbauer <patrick.simmelbauer@gmail.com>
2022-08-28 08:51:15 +0200

    add timed location

--- packages/strapi/src/api/live-location/controllers/live-location.js
+++ packages/strapi/src/api/live-location/controllers/live-location.js
@@ -10,7 +10,9 @@ module.exports = createCoreController("api::live-location.live-location", ({ str
   async update(ctx) {
     const curr = await strapi.service("api::live-location.live-location").find({ populate: "*" });
 
-    const newValues = [ctx.request.body, ...curr.values];
+    const newValues = [{ location: ctx.request.body, timestamp: Date.now() }, ...curr.values].map((timedLoc) => ({
+      ...timedLoc,
+    }));
 
     const result = await strapi
       .service("api::live-location.live-location")
