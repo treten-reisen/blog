f848aa558a5f0cc9bfb8580275674c37180064be
Patrick Simmelbauer <patrick.simmelbauer@gmail.com>
2022-11-26 18:25:27 +0100

    use maptiler maps

--- packages/astro/src/components/map/map-layer.tsx
+++ packages/astro/src/components/map/map-layer.tsx
@@ -1,8 +1,5 @@
-import { applyStyle } from "ol-mapbox-style"
-import MVT from "ol/format/MVT"
-import VectorTileLayer from "ol/layer/VectorTile"
-import VectorTileSource from "ol/source/VectorTile"
-import { useEffect, useRef } from "react"
+import { apply } from "ol-mapbox-style"
+import { useEffect } from "react"
 
 import { useMap } from "./map.context"
 
@@ -13,28 +10,9 @@ export type MapLayerProps = {
 const MapLayer = ({ apiKey }: MapLayerProps) => {
   const map = useMap()
 
-  const url =
-    window.devicePixelRatio > 1
-      ? `https://maps.geoapify.com/v1/tile/positron/{z}/{x}/{y}@2x.png?apiKey=${apiKey}`
-      : `https://maps.geoapify.com/v1/tile/positron/{z}/{x}/{y}.png?apiKey=${apiKey}`
-
-  const tileLayer = useRef(
-    new VectorTileLayer({
-      declutter: true,
-      source: new VectorTileSource({
-        format: new MVT(),
-        url,
-      }),
-    })
-  )
-
   useEffect(() => {
-    applyStyle(tileLayer.current, `https://maps.geoapify.com/v1/styles/positron/style.json?apiKey=${apiKey}`).then(
-      () => {
-        map.addLayer(tileLayer.current)
-      }
-    )
-  }, [tileLayer])
+    apply(map, `https://api.maptiler.com/maps/topo/style.json?key=${apiKey}`)
+  }, [apiKey, map])
 
   return <></>
 }
