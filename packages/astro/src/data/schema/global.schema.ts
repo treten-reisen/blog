23e1919fe4ba21e50b58c6165a85c50a11059af5
Patrick Simmelbauer <patrick.simmelbauer@gmail.com>
2023-02-27 23:37:06 +0100

    remove imprint and policy from astro schema

--- packages/astro/src/data/schema/global.schema.ts
+++ packages/astro/src/data/schema/global.schema.ts
@@ -1,7 +1,6 @@
 import { z } from "zod"
 
 import { transformStrapiImage } from "../image"
-import { markdownToHtml } from "../parse-markdown"
 
 import { strapiSeoSchema } from "./seo.schema"
 import { strapiSocialMediaSchema } from "./social-media.schema"
@@ -11,8 +10,6 @@ export const strapiGlobalSchema = strapiEntitySchema(
   z.object({
     siteName: z.string(),
     siteURL: z.string().url(),
-    imprint: z.string(),
-    dataPolicy: z.string(),
     favicon: strapiSingleSchema(strapiImageDataSchema),
     socialMedia: z.array(strapiSocialMediaSchema),
     defaultSeo: strapiSeoSchema,
@@ -22,8 +19,6 @@ export const strapiGlobalSchema = strapiEntitySchema(
   ...global,
   attributes: {
     ...global.attributes,
-    imprintHtml: String(await markdownToHtml(global.attributes.imprint)),
-    dataPolicyHtml: String(await markdownToHtml(global.attributes.dataPolicy)),
   },
 }))
 
