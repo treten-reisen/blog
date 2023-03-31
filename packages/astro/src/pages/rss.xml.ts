f40d8f0b4364bd60dfeac48c4fbfd063ed2dbd0e
Patrick Simmelbauer <patrick.simmelbauer@gmail.com>
2023-03-31 15:17:38 +0200

    add rss.xml

---
+++ packages/astro/src/pages/rss.xml.ts
@@ -0,0 +1,20 @@
+import rss from "@astrojs/rss"
+
+import { getArticleList } from "../data/get-article-list"
+
+export async function get() {
+  const articles = await getArticleList()
+
+  return rss({
+    title: "treten.reisen Blog",
+    description: "Meine Fahrradreise 8000 km um die Ostsee",
+    site: import.meta.env.SITE,
+    items: articles.data.map(article => ({
+      title: article.attributes.title,
+      link: import.meta.env.BASE_URL + article.attributes.slug,
+      pubDate: article.attributes.publishedAt || new Date(),
+    })),
+    customData: `<language>de-de</language>`,
+    stylesheet: "/rss/styles.xsl",
+  })
+}
