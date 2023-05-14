3e90a57147b989d919fc65445a68bee309a27ed3
Patrick Simmelbauer <patrick.simmelbauer@gmail.com>
2023-05-14 12:11:28 +0200

    use media gallery background blur

--- packages/astro/src/components/media-gallery/media-gallery-item.tsx
+++ packages/astro/src/components/media-gallery/media-gallery-item.tsx
@@ -13,20 +13,16 @@ const MediaGalleryItem = ({ file, label, objectFit, selected }: MediaGalleryItem
   return (
     <div role="tab" aria-selected={selected} aria-label={label} className="media-gallery__item">
       <a className="cursor-zoom-in" aria-label="Bild öffnen" href={file.attributes.url || undefined}>
-        <figure className="flex h-full flex-col items-center overflow-hidden">
-          {objectFit === "contain" && (
-            <img
-              src={file.htmlImage.src || undefined}
-              alt={file.htmlImage.alt || undefined}
-              className="left-0 top-0 -m-4 h-full w-full object-cover blur-lg"
-              aria-hidden="true"
-              loading="lazy"
-            />
-          )}
+        <figure
+          className="flex h-full flex-col items-center overflow-hidden bg-cover"
+          style={{
+            backgroundImage: file.htmlImage.src && objectFit === "contain" ? `url("${file.htmlImage.src}")` : undefined,
+          }}
+        >
           <img
             src={file.htmlImage.src || undefined}
             alt={file.htmlImage.alt || undefined}
-            className={classnames("absolute left-0 top-0 h-full w-full object-contain", {
+            className={classnames("absolute left-0 top-0 h-full w-full object-contain backdrop-blur", {
               "object-cover": objectFit === "cover",
               "object-contain": objectFit === "contain",
             })}
