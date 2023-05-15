ec25bbcdf5a58342f06d4130979ace9079f1139b
Patrick Simmelbauer <patrick.simmelbauer@gmail.com>
2023-05-15 16:23:29 +0200

    update provider

--- packages/astro/src/data/schema/strapi.schema.ts
+++ packages/astro/src/data/schema/strapi.schema.ts
@@ -28,7 +28,7 @@ export const strapiImageDataSchema = strapiEntitySchema(
     url: z.string().transform(url => new URL(url, import.meta.env.PUBLIC_STRAPI_API_URL).toString()),
     alternativeText: z.string(),
     width: z.nullable(z.number()).transform(width => width || undefined),
-    height: z.nullable(z.number()).transform(width => width || undefined),
+    height: z.nullable(z.number()).transform(height => height || undefined),
     caption: z.string().nullable(),
   })
 )
