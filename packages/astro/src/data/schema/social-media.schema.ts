c69f00001d57d0ff66e827f9da5c239395049cd1
Patrick Simmelbauer <patrick.simmelbauer@gmail.com>
2023-05-30 15:25:07 +0200

    add rss social media link

--- packages/astro/src/data/schema/social-media.schema.ts
+++ packages/astro/src/data/schema/social-media.schema.ts
@@ -4,7 +4,7 @@ export const strapiSocialMediaSchema = z.object({
   id: z.number(),
   username: z.string(),
   url: z.string().url(),
-  platform: z.enum(["facebook", "instagram", "twitter", "youtube", "komoot"]),
+  platform: z.enum(["facebook", "instagram", "twitter", "youtube", "komoot", "rss"]),
 })
 
 export type StrapiSocialMedia = z.infer<typeof strapiSocialMediaSchema>
