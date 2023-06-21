def76ec31f8d53f93981dae03f4a8345f26f5725
Patrick Simmelbauer <patrick.simmelbauer@gmail.com>
2023-06-21 18:41:39 +0200

    redesign card component

---
+++ packages/astro/src/date-format.ts
@@ -0,0 +1,16 @@
+import type { Locale } from "date-fns"
+import { de } from "date-fns/locale"
+
+const formatRelativeLocale = {
+  lastWeek: "'letzten' dddd",
+  yesterday: "'gestern'",
+  today: "'heute'",
+  tomorrow: "'morgen'",
+  nextWeek: "dddd",
+  other: "dd'.' LLLL y",
+}
+
+export const dateFnsLocale: Locale = {
+  ...de,
+  formatRelative: (token: keyof typeof formatRelativeLocale) => formatRelativeLocale[token],
+}
