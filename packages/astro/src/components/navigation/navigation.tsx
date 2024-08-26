4740d73b0e3cbc8be9b9ac6f5e6b48b60924190d
Patrick Simmelbauer <patrick.simmelbauer@gmail.com>
2024-08-26 14:55:54 +0200

    add skip to main content button

--- packages/astro/src/components/navigation/navigation.tsx
+++ packages/astro/src/components/navigation/navigation.tsx
@@ -26,51 +26,66 @@ const Navigation = ({ logoUrl, currentUrl, items }: NavigationProps) => {
   })
 
   return (
-    <div ref={elRef} className={classnames("fixed top-0 z-20 flex w-full flex-col", { "h-full": isOpen })}>
-      <div
-        className={classnames("h-14 bg-gray-700 bg-opacity-80 shadow-lg backdrop-blur-md", {
-          "transition-colors duration-500": !isOpen,
-        })}
-      >
-        <div className="flex h-full items-center justify-between px-6 md:container md:px-8">
-          <a href="/" tabIndex={0} title="Zur Startseite">
-            <img aria-label="treten.reisen Logo" className="h-6 self-start" src={logoUrl} alt="treten.reisen Logo" />
-          </a>
-          <button
-            className="text-xl text-gray-200 hover:text-gray-50 md:hidden"
-            title="Seitennavigation umschalten"
-            onClick={() => {
-              setOpen(open => !open)
-            }}
-            aria-haspopup="menu"
-            aria-expanded={isOpen}
-            aria-controls="navigation-menu"
+    <>
+      <div className="sr-only top-2 z-30 focus-within:not-sr-only focus-within:fixed focus-within:w-screen">
+        <div className="flex justify-center">
+          <a
+            href="#main-content"
+            className="mx-auto rounded-md border-2 border-gray-200 bg-gray-900 p-2 px-4 text-gray-200"
           >
-            <FontAwesomeIcon icon={isOpen ? faXmark : faBars} />
-          </button>
-          <div className="hidden md:block">
-            <NavMenu orientation="horizontal" items={items} currentUrl={currentUrl} color="bright" />
-          </div>
+            Zum Haupt-Inhalt springen
+          </a>
         </div>
       </div>
-      {isOpen && (
-        <>
-          <div
-            className={classnames("flex justify-end bg-gray-50 px-responsive py-6 text-right shadow-2xl md:container", {
-              "text-lg": isOpen,
-            })}
-            id="navigation-menu"
-            role="menu"
-            aria-label="Navigation"
-            aria-modal="true"
-            ref={el => el?.focus()}
-          >
-            <NavMenu orientation="vertical" items={items} currentUrl={currentUrl} color="dark" />
+      <div ref={elRef} className={classnames("fixed top-0 z-20 flex w-full flex-col", { "h-full": isOpen })}>
+        <div
+          className={classnames("h-14 bg-gray-700 bg-opacity-80 shadow-lg backdrop-blur-md", {
+            "transition-colors duration-500": !isOpen,
+          })}
+        >
+          <div className="flex h-full items-center justify-between px-6 md:container md:px-8">
+            <a href="/" tabIndex={0} title="Zur Startseite">
+              <img aria-label="treten.reisen Logo" className="h-6 self-start" src={logoUrl} alt="treten.reisen Logo" />
+            </a>
+            <button
+              className="text-xl text-gray-200 hover:text-gray-50 md:hidden"
+              title="Seitennavigation umschalten"
+              onClick={() => {
+                setOpen(open => !open)
+              }}
+              aria-haspopup="menu"
+              aria-expanded={isOpen}
+              aria-controls="navigation-menu"
+            >
+              <FontAwesomeIcon icon={isOpen ? faXmark : faBars} />
+            </button>
+            <div className="hidden md:block">
+              <NavMenu orientation="horizontal" items={items} currentUrl={currentUrl} color="bright" />
+            </div>
           </div>
-          <div className="z-50 flex-1 bg-gray-900 bg-opacity-20 backdrop-blur-lg"></div>
-        </>
-      )}
-    </div>
+        </div>
+        {isOpen && (
+          <>
+            <div
+              className={classnames(
+                "flex justify-end bg-gray-50 px-responsive py-6 text-right shadow-2xl md:container",
+                {
+                  "text-lg": isOpen,
+                }
+              )}
+              id="navigation-menu"
+              role="menu"
+              aria-label="Navigation"
+              aria-modal="true"
+              ref={el => el?.focus()}
+            >
+              <NavMenu orientation="vertical" items={items} currentUrl={currentUrl} color="dark" />
+            </div>
+            <div className="z-50 flex-1 bg-gray-900 bg-opacity-20 backdrop-blur-lg"></div>
+          </>
+        )}
+      </div>
+    </>
   )
 }
 
