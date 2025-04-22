521850aebfaff53ecb94ace26d31913c46f06fd2
Patrick Simmelbauer <patrick.simmelbauer@gmail.com>
2025-04-23 01:47:48 +0200

    update strapi 4.15.5

--- packages/strapi/config/middlewares.js
+++ packages/strapi/config/middlewares.js
@@ -1,4 +1,5 @@
 module.exports = [
+  "strapi::logger",
   "strapi::errors",
   {
     name: "strapi::security",
@@ -17,7 +18,6 @@ module.exports = [
   },
   "strapi::cors",
   "strapi::poweredBy",
-  "strapi::logger",
   "strapi::query",
   "strapi::body",
   "strapi::session",
